90feb2603f2e1ebd6dd2c4636cdaa5a43f5d9ee6
Pablo Bauszat <p.bauszat@tudelft.nl>
2022-04-28 12:28:16 +0200

    Moved the visualization to a separate module.

--- visualize.py
+++ visualize.py
@@ -1,17 +1,15 @@
 import csv
-import cv2 as cv
 import evaluation as eval
 import siminet
-import time
 import video_utils
 
 from fusion import ScoreFusionModule
 from opts import parse_opts
 from stats import FusionStats
+from visualizer import VisualizerOpenCV
 
 _FUSION_METHOD = "exponential_smoothing"
 _VIDEO_LEVEL_METRIC = False
-_EXTENSION = "avi"
 # _VIDEOS_LST = ["0296-M", "0309-L", "312-L", "0322-L"]
 _VIDEOS_LST = ["0322-L"]
 
@@ -29,85 +27,25 @@ def read_txt_labels(class_idx):
     return txt_lables
 
 
-def show(opts, video, local_pred_actions, remote_pred_actions, fusion_pred_actions, true_actions, txt_labels):
-    video_file = opts.datasets_dir + '/' + opts.datasets + \
-                 f'/videos/{video}.{_EXTENSION}'
-
-    cap = cv.VideoCapture(video_file)
-    if not (cap.isOpened()):
-        print("Cannot open ", video_file)
-        return
-
-    frame_id = 0
-    left_margin = 10
-    gap_margin = 75
-    while (True):
-        try:
-            ret, frame = cap.read()
-            if ret is False:
-                break
-
-            if frame_id >= (opts.window_size // 2) < len(local_pred_actions):
-                local_action_txt = txt_labels[local_pred_actions[frame_id]]
-                cv.putText(frame, f"Local: {local_action_txt}", (left_margin, gap_margin * 1),
-                           cv.FONT_HERSHEY_SIMPLEX, 1.5, (0, 0, 255), 1)
-                remote_action_txt = txt_labels[remote_pred_actions[frame_id]]
-                cv.putText(frame, f"Remote: {remote_action_txt}", (left_margin, gap_margin * 2),
-                           cv.FONT_HERSHEY_SIMPLEX, 1.5, (0, 255, 0), 1)
-                fusion_action_txt = txt_labels[fusion_pred_actions[frame_id]]
-                cv.putText(frame, f"Clownfish: {fusion_action_txt}", (left_margin, gap_margin * 3),
-                           cv.FONT_HERSHEY_SIMPLEX, 1.5, (255, 0, 0), 1)
-                true_action_txt = txt_labels[true_actions[frame_id]]
-                cv.putText(frame, f"GT: {true_action_txt}", (left_margin, gap_margin * 4),
-                           cv.FONT_HERSHEY_SIMPLEX, 1.5, (0, 0, 0), 1)
-            else:
-                # The number of frames in the videos could be higher than the number of predictions
-                # because we do not take the very last background action into consideration in the
-                # ground truth actions itself. That's how the ground truth json file is generated.
-                # But the clownfish, local and remote predict all the frames. So, not an issue.
-                # break
-                pass
-
-            cv.imshow('frame', frame)
-
-            if cv.waitKey(1) & 0xFF == ord('q'):
-                break
-            frame_id += 1
-            time.sleep(30 * 1e-3)
-        except Exception as e:
-            print(f"Exception {str(e)}")
-            break
-    print(
-        f"Total frames: {frame_id}, {len(local_pred_actions)}, {len(true_actions)}")
-    cap.release()
-    cv.destroyAllWindows()
-
-
-def get_pred_actions(opts, video, labels):
-    use_siminet = (opts.sim_method ==
-                   "siminet" or opts.corr_at_transition or opts.corr_per_window)
+def get_predicted_actions(opts, video, labels):
+    use_siminet = opts.sim_method == "siminet" or opts.corr_at_transition or opts.corr_per_window
 
     # Find file paths where scores are dumped
     local_score_file = opts.local_scores_dir + "/" + video
     remote_score_file = opts.remote_scores_dir + "/" + video
-    p_local_window = []
-    p_remote_window = []
     p_local_feature_window = []  # for siminet
-    if use_siminet:
-        local_feature_file = opts.local_scores_dir + "/raw_features/" + video
+
     try:
         # Read prediction scores from the dump file
-        p_local_window = video_utils.get_scores(
-            local_score_file, use_softmax=False, flow=False)
-        p_remote_window = video_utils.get_scores(
-            remote_score_file, use_softmax=False, flow=False)
+        p_local_window = video_utils.get_scores(local_score_file, use_softmax=False, flow=False)
+        p_remote_window = video_utils.get_scores(remote_score_file, use_softmax=False, flow=False)
 
         if use_siminet:
-            p_local_feature_window = video_utils.get_scores(
-                local_feature_file, use_softmax=False, flow=False)
-            assert len(p_local_window) == len(
-                p_local_feature_window), "Softmax and Raw features windows does ot match"
+            local_feature_file = opts.local_scores_dir + "/raw_features/" + video
+            p_local_feature_window = video_utils.get_scores(local_feature_file, use_softmax=False, flow=False)
+            assert len(p_local_window) == len(p_local_feature_window), "Softmax and Raw features windows does ot match"
     except:
+        # todo: Never use an empty except.
         return None
 
     # Get fusion prediction scores
@@ -128,22 +66,22 @@ def get_pred_actions(opts, video, labels):
                                 p_remote_window, p_perceived_window,
                                 p_local_feature_window)
     metric = eval_metric.get_top1_accuracy_frame()
-    print(
-        f"Accuracy: {video}: {metric[eval.LOCAL]}, {metric[eval.REMOTE]}, {metric[eval.PERCEIVED]}")
-    local_pred_actions = eval_metric.y_pred_per_frame_lst[eval.LOCAL]
-    remote_pred_actions = eval_metric.y_pred_per_frame_lst[eval.REMOTE]
-    fusion_pred_actions = eval_metric.y_pred_per_frame_lst[eval.PERCEIVED]
+    print(f"Accuracy: {video}: {metric[eval.LOCAL]}, {metric[eval.REMOTE]}, {metric[eval.PERCEIVED]}")
+    local_predicted_actions = eval_metric.y_pred_per_frame_lst[eval.LOCAL]
+    remote_predicted_actions = eval_metric.y_pred_per_frame_lst[eval.REMOTE]
+    fusion_predicted_actions = eval_metric.y_pred_per_frame_lst[eval.PERCEIVED]
     true_actions = eval_metric.y_true_per_frame_lst
 
-    return (local_pred_actions, remote_pred_actions, fusion_pred_actions, true_actions)
+    return local_predicted_actions, remote_predicted_actions, fusion_predicted_actions, true_actions
 
 
 def main():
+    # Parse arguments (options)
     opts = parse_opts()
 
+    # Read labels
     # label_file = opts.datasets_dir + '/' + opts.datasets + '/splits/pkummd.json'
-    label_file = opts.datasets_dir + '/' + opts.datasets + \
-                 '/splits/pkummd_cross_subject_background.json'
+    label_file = opts.datasets_dir + '/' + opts.datasets + '/splits/pkummd_cross_subject_background.json'
     class_file = opts.datasets_dir + '/' + opts.datasets + '/splits/classInd.txt'
     labels = video_utils.VideoLabels(label_file, class_file, opts.datasets)
     txt_labels = read_txt_labels(class_file)
@@ -159,15 +97,11 @@ def main():
 
     # _VIDEOS_LST = labels.labels_key
     for video in _VIDEOS_LST:
-        ret = get_pred_actions(opts=opts, video=video, labels=labels)
+        ret = get_predicted_actions(opts=opts, video=video, labels=labels)
         if ret is not None:
-            (local_pred_actions,
-             remote_pred_actions,
-             fusion_pred_actions,
-             true_actions) = ret
-
-            show(opts, video, local_pred_actions, remote_pred_actions,
-                 fusion_pred_actions, true_actions, txt_labels)
+            local_predicted_actions, remote_predicted_actions, fusion_predicted_actions, true_actions = ret
+            visualizer = VisualizerOpenCV(opts, video)
+            visualizer.show(local_predicted_actions, remote_predicted_actions, fusion_predicted_actions, true_actions, txt_labels)
 
 
 if __name__ == '__main__':
---
+++ visualizer/opencv_visualizer.py
@@ -0,0 +1,80 @@
+"""
+A simple visualizer using OpenCV.
+"""
+import argparse
+import cv2 as cv
+import numpy as np
+import time
+
+
+class VisualizerOpenCV:
+    # Define video extension and FPS
+    VIDEO_EXTENSION: str = "avi"
+    FPS: float = 30.0
+    # Define Action type
+    Action = np.int64
+
+    def __init__(self, opts: argparse.Namespace, video: str):
+        self._video_file: str = opts.datasets_dir + "/" + opts.datasets + f"/videos/{video}.{VisualizerOpenCV.VIDEO_EXTENSION}"
+        self._window_size: int = opts.window_size
+        self._window_id = "video"
+
+    def show(self,
+             local_predicted_actions: list[Action],
+             remote_predicted_actions: list[Action],
+             fusion_predicted_actions: list[Action],
+             true_actions: list[int],
+             txt_labels: dict[int, str]):
+
+        cap = cv.VideoCapture(self._video_file)
+        if not (cap.isOpened()):
+            print("Cannot open ", self._video_file)
+            return
+
+        frame_id = 0
+        left_margin = 10
+        gap_margin = 75
+        while True:
+            try:
+                ret, frame = cap.read()
+                if ret is False:
+                    break
+
+                if frame_id >= (self._window_size // 2) < len(local_predicted_actions):
+                    local_action = local_predicted_actions[frame_id]
+                    remote_action = remote_predicted_actions[frame_id]
+                    fusion_action = fusion_predicted_actions[frame_id]
+                    true_action = true_actions[frame_id]
+
+                    local_action_txt = txt_labels[local_action]
+                    remote_action_txt = txt_labels[remote_action]
+                    fusion_action_txt = txt_labels[fusion_action]
+                    true_action_txt = txt_labels[true_action]
+
+                    cv.putText(frame, f"Local: {local_action_txt}", (left_margin, gap_margin * 1), cv.FONT_HERSHEY_SIMPLEX, 1.5, (0, 0, 255), 1)
+                    cv.putText(frame, f"Remote: {remote_action_txt}", (left_margin, gap_margin * 2), cv.FONT_HERSHEY_SIMPLEX, 1.5, (0, 255, 0), 1)
+                    cv.putText(frame, f"Clownfish: {fusion_action_txt}", (left_margin, gap_margin * 3), cv.FONT_HERSHEY_SIMPLEX, 1.5, (255, 0, 0), 1)
+                    cv.putText(frame, f"GT: {true_action_txt}", (left_margin, gap_margin * 4), cv.FONT_HERSHEY_SIMPLEX, 1.5, (0, 0, 0), 1)
+                else:
+                    # The number of frames in the videos could be higher than the number of predictions
+                    # because we do not take the very last background action into consideration in the
+                    # ground truth actions itself. That's how the ground truth json file is generated.
+                    # But the clownfish, local and remote predict all the frames. So, not an issue.
+                    # break
+                    pass
+
+                cv.imshow(self._window_id, frame)
+                cv.setWindowTitle(self._window_id, f"frame {frame_id} - fps: {VisualizerOpenCV.FPS:.1f}")
+                if cv.waitKey(1) & 0xFF == ord("q"):
+                    break
+
+                frame_id += 1
+                time.sleep(VisualizerOpenCV.FPS * 1e-3)
+
+            except Exception as e:
+                print(f"Exception {str(e)}")
+                break
+
+        print(f"Total frames: {frame_id}, {len(local_predicted_actions)}, {len(true_actions)}")
+        cap.release()
+        cv.destroyAllWindows()
